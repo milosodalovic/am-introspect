412c6e010f1964b728153f5417a6f1dc5805c14b
Masa <masa.boskovic@codeus.me>
2019-06-12 15:47:09 +0200

    Learn More component almost ready

--- src/components/LearnMore.vue
+++ src/components/LearnMore.vue
@@ -1,3 +1,120 @@
 <template>
-    
+    <section id="one">
+		<div class="inner">
+		    <header>
+				<h2>Magna Etiam Lorem</h2>
+			</header>
+				<p>Suspendisse mauris. Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper vehicula. Integer adipiscin sem. Nullam quis massa sit amet nibh viverra malesuada. Nunc sem lacus, accumsan quis, faucibus non, congue vel, arcu, erisque hendrerit tellus. Integer sagittis. Vivamus a mauris eget arcu gravida tristique. Nunc iaculis mi in ante.</p>
+					<ul class="actions">
+						<li><a href="#" class="button alt">Learn More</a></li>
+		</ul>
+                    
+		</div>
+	</section>
 </template>
+
+<style>
+h2 {
+    padding: 0;
+    border: 0;
+    vertical-align: baseline;
+    font-weight: 700;
+    line-height: 1.5;
+    margin: 0 0 35px 0;
+    text-transform: uppercase;
+    text-decoration: none;
+    font-size: 1.75em;
+    color: #e5474b;
+}
+#one {
+    padding: 6em 0 4em 0;
+    margin: 0px;
+    font-size: 15pt;
+    font-weight: 300;
+    line-height: 2;
+    font-family: "Lato", sans-serif;
+}
+
+@media screen and (max-width: 980px) {
+    #one {
+        padding: 4em 0 2em 0;
+    }
+}
+
+@media screen and (max-width: 736px) {
+    #one {
+        padding: 2em 0 0.1em 0;
+    }
+}
+@media screen and (max-width: 480px) {
+    #one {
+        font-size: 12pt;
+    }
+}
+@media screen and (max-width: 980px) {
+    #one {
+        font-size: 12pt;
+    }
+}
+@media screen and (max-width: 1280px) {
+    #one {
+        font-size: 13pt;
+    }
+}
+@media screen and (max-width: 1680px) {
+    #one {
+        font-size: 13pt;
+    }
+}
+section {
+    vertical-align: baseline;
+}
+
+section .inner {
+    max-width: 65em;
+    width: calc(100% - 6em);
+    margin: 0 auto;
+    text-align: left;
+}
+
+@media screen and (max-width: 480px) {
+    section .inner {
+        max-width: 90%;
+        width: 90%;
+    }
+}
+header,
+p {
+    margin: 0 0 2em 0;
+    color: #111111;
+}
+ul {
+    padding-left: 0;
+    margin: 0 0 2em 0;
+    font-family: "Lato", sans-serif;
+
+    list-style: none;
+}
+li {
+    padding: 0 1em 0 0;
+}
+
+.button.alt {
+    background-color: transparent;
+    box-shadow: inset 0 0 0 2px #e5474b;
+    color: #e5474b !important;
+    display: inline-block;
+    font-size: 0.8em;
+    font-weight: 700;
+    height: 3.5em;
+    line-height: 3.65em;
+    padding: 0 2.25em;
+    text-align: center;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-family: "Lato", sans-serif;
+}
+.button.alt:hover {
+    background: #fceced;
+}
+</style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
     <header id="header">
         <div class="inner">
-            <a href="index.html" class="logo">introspect</a>
-            <nav id="nav" class="test">
+            <a  class="logo">introspect</a>
+            <nav id="nav" >
                 <a href="index.html">Home</a>
                 <a href="generic.html">Generic</a>
                 <a href="elements.html">Elements</a>
@@ -18,11 +18,154 @@ export default {};
 
 
 <style lang="scss">
-@import url(../assets/css/main.css);
+@import url(../assets/css/font-awesome.min.css);
+@import url("https://fonts.googleapis.com/css?family=Lato:300,400,700|Pacifico");
+#header {
+    background: #1c1c1c;
+    color: #d5d5d5;
+    cursor: default;
+    height: 5em;
+    left: 0;
+    line-height: 5em;
+    position: fixed;
+    text-align: right;
+    top: 0;
+    width: 100%;
+    z-index: 10001;
+}
 
-.test {
-    @media screen and (max-width: 600px) {
-        visibility: hidden;
+#header .inner {
+    max-width: 65em;
+    width: calc(100% - 6em);
+    margin: 0 auto;
+    position: relative;
+}
+@media screen and (max-width: 480px) {
+    #header .inner {
+        max-width: 90%;
+        width: 90%;
     }
+}
+
+#header .logo {
+    font-family: "Pacifico", cursive;
+    display: inline-block;
+    height: inherit;
+    left: 0;
+    line-height: inherit;
+    margin: 0;
+    padding: 0;
+    position: absolute;
+    top: 0;
+    color: #e5474b;
+    font-size: 1.75em;
+    text-transform: none;
+    font-weight: normal;
+    padding: 0;
+}
+
+#header .logo:hover {
+    color: rgba(229, 71, 75, 0.75);
+}
+
+#header a {
+    -moz-transition: color 0.2s ease-in-out;
+    -webkit-transition: color 0.2s ease-in-out;
+    -ms-transition: color 0.2s ease-in-out;
+    transition: color 0.2s ease-in-out;
+    display: inline-block;
+    padding: 0 2em;
+    color: #ffffff;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 0.85em;
+}
+
+#header a:hover {
+    color: rgba(255, 255, 255, 0.75);
+}
+
+#header a:last-child {
+    padding-right: 0em;
+}
+
+@media screen and (max-width: 736px) {
+    #header a {
+        padding: 0 0.5em;
+    }
+}
+
+@media screen and (max-width: 480px) {
+    #header {
+        min-width: 320px;
+    }
+}
+
+/* Menu */
+
+#nav {
+    display: inline-block;
+}
+
+@media screen and (max-width: 980px) {
+    #nav {
+        display: none;
+    }
+}
+
+.navPanelToggle {
+    position: fixed;
+    text-decoration: none;
+    height: 4em;
+    right: 3em;
+    top: 0.75em;
+    width: 4em;
+    display: none;
+    z-index: 10002;
+}
+
+.navPanelToggle:before {
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    font-family: FontAwesome;
+    font-style: normal;
+    font-weight: normal;
+    text-transform: none !important;
+    color: #fff;
+    display: block;
+    font-size: 16px;
+    height: 2.25em;
+    left: 0.5em;
+    line-height: 2.25em;
+    position: absolute;
+    text-align: center;
+    top: 0.5em;
+    width: 3.5em;
+}
+
+@media screen and (max-width: 980px) {
+    .navPanelToggle {
+        display: block;
+    }
+}
+
+@media screen and (max-width: 736px) {
+    .navPanelToggle {
+        right: 1em;
+    }
+}
+
+.navPanelToggle *:before {
+    color: #fff;
+    display: block;
+    font-size: 16px;
+    height: 2.25em;
+    left: 0.5em;
+    line-height: 2.25em;
+    position: absolute;
+    text-align: center;
+    top: 0.5em;
+    width: 3.5em;
 }
 </style>
