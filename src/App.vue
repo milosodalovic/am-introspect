c6c76e81d128a07ca70d6febdb923534f705e695
Mihailo Obradović <mihailo.obradovic@codeus.me>
2019-06-12 13:27:28 +0200

    Created base structure.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <navbar></navbar>
+    <hero></hero>
+    <learn-more></learn-more>
+    <image-links></image-links>
+    <icon-links></icon-links>
+    <intro-form></intro-form></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Hero from "./components/Hero.vue";
+import IconLinks from "./components/IconLinks.vue";
+import ImageLinks from "./components/ImageLinks.vue";
+import IntroForm from "./components/IntroForm.vue";
+import LearnMore from "./components/LearnMore.vue";
+import Navbar from "./components/Navbar.vue";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    name: "app",
+    components: {
+        IntroForm,
+        Hero,
+        IconLinks,
+        ImageLinks,
+        LearnMore,
+        Navbar
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+    <header id="header">
+        <div class="inner">
+            <a href="index.html" class="logo">introspect</a>
+            <nav id="nav" class="test">
+                <a href="index.html">Home</a>
+                <a href="generic.html">Generic</a>
+                <a href="elements.html">Elements</a>
+            </nav>
+        </div>
+        <a href="#menu" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+    </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style lang="scss">
+@import url(../assets/css/main.css);
+
+.test {
+    @media screen and (max-width: 600px) {
+        visibility: hidden;
+    }
+}
+</style>
